deda78081b596b60c5503a1b82923a53608f709f
Alberto Beltran <albertobeltran@protonmail.com>
2022-06-21 11:00:04 -0500

    set the 3D scene lights

--- src/index.js
+++ src/index.js
@@ -9,4 +9,23 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-renderer.render( scene, camera );
+// Setting the scene lights
+const ambient_light = new THREE.AmbientLight(0x404040);
+const direction_light = new THREE.DirectionalLight(0xfff, 1);
+ambient_light.add(direction_light);
+scene.add(ambient_light);
+
+// Cube
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshPhongMaterial( { color: 0x090979 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+camera.position.z = 5;
+
+function animate() {
+    cube.rotation.x += 0.05;
+    cube.rotation.y += 0.05;
+	requestAnimationFrame( animate );
+	renderer.render( scene, camera );
+}
+animate();
